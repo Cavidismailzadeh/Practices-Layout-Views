5bf16f72c25c2bac38767ef80b5fda0acc5d350f
Javid A. Ismayilzada <javidai@code.local>
2023-04-03 21:47:09 +0400

    add

---
+++ Practices-Layout-Views/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practices_Layout_Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //ViewData["names"] = new string[] { "Elcan", "Pervin", "Aqshin" };
+
+            //ViewBag.numbers = new int[] { 10, 20, 30, 40 };
+
+
+            //string name = "Konul";
+
+            //List<int> nums = new List<int>() { 1, 2, 3, 4, 5 };
+
+
+
+            return View();
+        }
+    }
+}
