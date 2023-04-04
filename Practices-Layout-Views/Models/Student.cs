4fe9e08f2a6aebd088a62c07b8ac75dba4acafea
unknown <javidai@code.edu.az>
2023-04-04 04:07:45 +0400

    FrontToBack

--- Practices-Layout-Views/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Practices_Layout_Views.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; }
-        public int Age { get; set; }
-    }
-}
--- Practices-Layout-Views/ViewModels/Product/ProductVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Practices_Layout_Views.ViewModels.Product
-{
-    public class ProductVM
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
